c71af0a0c9ac6909f6a5bba8ae58349c4d41269c
Hailey Sanchez <haileysofia2002@gmail.com>
2025-05-17 14:55:39 -0500

    Create base table

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import DataTable from './components/Table'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <DataTable />
+      </div>
+    </>
+  )
+}
+
+export default App
